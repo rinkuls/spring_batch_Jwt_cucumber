4138c9c5e0299fb7a436d001104700ae295b5855
rinkul.sharma <rinkul.sharma@accenture.com>
2024-12-18 14:46:19 +0100

    updated DAO

--- src/main/java/com/spring/batch/model/DAOUser.java
+++ src/main/java/com/spring/batch/model/DAOUser.java
@@ -15,7 +15,7 @@ import javax.persistence.*;
 public class DAOUser {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     @Column
     private String username;
